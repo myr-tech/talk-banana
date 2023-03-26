be36e0e586767e209f6c308a04f448349941260a
myr-tech <mayur.surbhalavi@gmail.com>
2023-03-26 22:41:30 +0530

    feat

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 // var username = prompt("give me your username")
 // alert("this script works " + username);
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate)
