fcd33d20da9b9bb69838b01b42a6ac3888217c10
myr-tech <mayur.surbhalavi@gmail.com>
2023-03-28 16:15:54 +0530

    js file created

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// var username = prompt("give me your username")
-// alert("this script works " + username);
+
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate)
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    console.log("clicked")
+})
